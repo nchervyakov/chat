8d8c6816fa315ab5313fbb85534538fda5a9648f
Nikolay Chervyakov <nick.chervyakov@gmail.com>
2015-05-06 12:47:37 +0600

    Fix Migration.

--- app/DoctrineMigrations/Version20150430162314.php
+++ app/DoctrineMigrations/Version20150430162314.php
@@ -20,7 +20,7 @@ class Version20150430162314 extends AbstractMigration
 
         $this->addSql('CREATE TABLE message_complaint (id INT AUTO_INCREMENT NOT NULL, message_id INT NOT NULL, model_id INT NOT NULL, resolved_by INT DEFAULT NULL, status VARCHAR(16) DEFAULT \'open\' NOT NULL, date_added DATETIME DEFAULT NULL, date_updated DATETIME DEFAULT NULL, added_by_ip VARCHAR(45) NOT NULL, UNIQUE INDEX UNIQ_359E3483537A1329 (message_id), INDEX IDX_359E34837975B7E7 (model_id), INDEX IDX_359E348357EB21F9 (resolved_by), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE message_complaint ADD CONSTRAINT FK_359E3483537A1329 FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE message_complaint ADD CONSTRAINT FK_359E34837975B7E7 FOREIGN KEY (model_id) REFERENCES users (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE message_complaint ADD CONSTRAINT FK_359E34837975B7E7 FOREIGN KEY (model_id) REFERENCES users (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE message_complaint ADD CONSTRAINT FK_359E348357EB21F9 FOREIGN KEY (resolved_by) REFERENCES users (id) ON DELETE SET NULL');
         $this->addSql('ALTER TABLE messages ADD added_by_ip VARCHAR(45) NOT NULL, ADD notification_type VARCHAR(16) DEFAULT \'info\'');
     }
